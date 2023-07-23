09ccb23a61272e3ee11fb3b3b363c167ccc08b58
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-07-22 21:27:37 -0500

    Add files via upload

--- server.py
+++ server.py
@@ -1,26 +1,26 @@
-from flask import Flask  # Import Flask to allow us to create our app
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
 def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+    return 'Hello World!'
 
-@app.route('/success')
+@app.route('/dojo')
 def success():
-  return "success"
+    return "Dojo!"
 
-@app.route('/hello/<name>') # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
+@app.route('/say/<name>')
 def hello(name):
-    print(name)
-    return "Hello, " + name
+    return "Hi, " + name
 
-@app.route('/users/<username>/<id>') # for a route '/users/____/____', two parameters in the url get passed as username and id
-def show_user_profile(username, id):
-    print(username)
-    print(id)
-    return "username: " + username + ", id: " + id
+@app.route('/repeat/<int:num>/<string>')
+def repeat_string(string, num):
+    return string * num
 
+@app.route('/<path:path>')
+def catch_all(path):
+    return "Sorry! No response. Try again."
 
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
-
-app.run(debug=True, host="localhost", port=8000)
+if __name__ == "__main__":
+    app.run(debug=True)
