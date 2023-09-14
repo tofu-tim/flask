5a45644ec935152f7aa72ad646e451397dad1d33
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-09-13 22:31:23 -0500

    Add files via upload

--- counter.py
+++ counter.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, request
 
 app = Flask(__name__)
 app.secret_key = 'kickflip'
@@ -19,6 +19,15 @@ def reset():
 @app.route('/add_two_visits')
 def add_two_visits():
     session['counter'] += 1
+    return redirect('/')
+
+@app.route('/increase_visits', methods=['POST'])
+def increase_visits():
+    try:
+        count_to_add = int(request.form.get('count_to_add'))
+        session['counter'] += (count_to_add - 1)
+    except ValueError:
+        pass
     return redirect('/')
 
 if __name__ == '__main__':
