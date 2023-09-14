d650d7a4abeb2ac562f82a34a61b473c0d87605f
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-09-13 21:51:50 -0500

    Add files via upload

--- checkerboard.py
+++ checkerboard.py
@@ -1,24 +1,26 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-print(__name__)
-
 @app.route('/')
 def index():
-    return render_template("index.html", rows=8, cols=8, color1="black", color2="white")
+    return render_template("index.html", input1=8, input2=8)
 
-@app.route('/<int:rows>')
-def checkerboard_rows(rows):
-    return render_template("index.html", rows=rows, cols=8, color1="black", color2="white")
+@app.route('/<int:input1>')
+def checkerboard_rows(input1):
+    return render_template("index.html", input1=input1, input2=8)
 
-@app.route('/<int:rows>/<int:cols>')
-def checkerboard_custom(rows, cols):
-    return render_template("index.html", rows=rows, cols=cols, color1="black", color2="white")
+@app.route('/<int:input1>/<int:input2>')
+def checkerboard_custom(input1, input2):
+    return render_template("index.html", input1=input1, input2=input2)
 
-@app.route('/<int:rows>/<int:cols>/<color1>/<color2>')
-def checkerboard_custom_colors(rows, cols, color1, color2):
-    return render_template("index.html", rows=rows, cols=cols, color1=color1, color2=color2)
+@app.route('/<int:input1>/<int:input2>/<color1>/<color2>')
+def checkerboard_custom_colors(input1, input2, color1, color2):
+    return render_template("index.html", input1=input1, input2=input2, color1=color1, color2=color2)
+
+@app.route('/static/style.css')
+def style():
+    return app.send_static_file('style.css')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -26,4 +28,3 @@ def page_not_found(e):
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
-
